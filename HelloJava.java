047e6efe3c1a51da9c47b412475ca97c96ae9122
isuri <isuriwgunaratne@gmail.com>
2023-07-28 09:23:36 +0530

    Initialcommit

---
+++ HelloJava.java
@@ -0,0 +1,10 @@
+public class HelloJava{
+    public static void main(String[] args) {
+        int name = isuri;
+        System.out.println(name);
+        String NIC = 976280202V;
+        System.out.println(NIC);
+
+
+    }
+}
